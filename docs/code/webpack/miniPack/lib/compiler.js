fea096dbf158a4a01c5b2cab649a7c2599a5beca
王勇 <wangyong01@xdfzx.com>
2023-09-20 17:22:27 +0800

    feat: add build module and emit file

--- docs/code/webpack/miniPack/lib/compiler.js
+++ docs/code/webpack/miniPack/lib/compiler.js
@@ -1,6 +1,21 @@
+const parser = require("./parser");
+const resolve = require("enhanced-resolve");
 class Compiler {
-  constructor(config) {}
-  run() {}
+  constructor(config) {
+    this.config = config;
+    this.entry = config.entry;
+    this.output = config.output;
+    this.execPath = process.cwd(); // 当前工作目录
+    this.modules = Object.create(null); // 基于module依赖得到集合
+  }
+  run() {
+    this.buildModule(resolve.sync(this.execPath, this.entry));
+    this.emitFile();
+  }
+  // 构建依赖关系图
+  buildModule(path) {}
+  // 生成打包文件
+  emitFile() {}
 }
 
 module.exports = Compiler;
