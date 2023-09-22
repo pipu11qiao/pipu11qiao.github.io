a823cff28795f18e27e3396f5c920c97a7e892f4
王勇 <wangyong01@xdfzx.com>
2023-09-22 19:30:26 +0800

    feat: add valtio debug

--- docs/code/webpack/example/1_bootstrap/webpack.config.js
+++ docs/code/webpack/example/1_bootstrap/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  entry: "./src/index.js",
   //   entry: {
   //     a: "./src/a.js",
   //     b: "./src/b.js",
   //   },
-  entry: {
-    A: ["./src/entry1.js", "./src/entry2.js"],
-  },
+  // entry: {
+  //   A: ["./src/entry1.js", "./src/entry2.js"],
+  // },
   output: {
     filename: "[name].js",
     path: path.join(__dirname, "./dist"),
